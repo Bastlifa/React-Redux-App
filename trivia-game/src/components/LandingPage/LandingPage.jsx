663d87967f0f6413d0a2687ab3604f17079c2dfc
Eli Sacks <bastlifa@gmail.com>
2019-08-15 18:26:40 -0400

    fixed landing page text

--- trivia-game/src/components/LandingPage/LandingPage.jsx
+++ trivia-game/src/components/LandingPage/LandingPage.jsx
@@ -5,7 +5,7 @@ const LandingPage = () =>
     return (
         <>
             <h1>Welcome to Trivia.</h1>
-            <h2>Select a category, and click "New Game"</h2>
+            <h2>Select a category, and click "Start Game"</h2>
         </>
     )
 }
